a19f6e22721e7918f2a8b8766567b7662b8a0183
Amy <chengamy5@gmail.com>
2020-08-12 00:18:34 -0700

    Xcode for facts about me

---
+++ Mini Project 1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mini Project 1
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Amy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    //@IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var labelAmy: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+}
+
